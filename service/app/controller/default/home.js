4d7ca4a7c13f47f146971ee843576675a0f77915
xhc1302 <xhc1302@gmail.com>
2020-10-16 09:38:38 +0800

    add service

---
+++ service/app/controller/default/home.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class HomeController extends Controller {
+  async index() {
+    this.ctx.body='hi home index'
+  }
+
+  async getArticleList(){
+
+    let sql = 'SELECT article.id as id,'+
+              'article.title as title,'+
+              'article.introduce as introduce,'+
+              "FROM_UNIXTIME(article.addTime,'%Y-%m-%d' ) as addTime,"+
+              'article.view_count as view_count ,'+
+              '.type.typeName as typeName '+
+              'FROM article LEFT JOIN type ON article.type_id = type.Id order by article.id desc'
+ 
+     const results = await this.app.mysql.query(sql)
+ 
+     this.ctx.body={
+         data:results
+     }
+ }
+
+ async getArticleById(){
+  //先配置路由的动态传值，然后再接收值
+  let id = this.ctx.params.id
+
+  let sql = 'SELECT article.id as id,'+
+  'article.title as title,'+
+  'article.introduce as introduce,'+
+  'article.article_content as article_content,'+
+  "FROM_UNIXTIME(article.addTime,'%Y-%m-%d' ) as addTime,"+
+  'article.view_count as view_count ,'+
+  'type.typeName as typeName ,'+
+  'type.id as typeId '+
+  'FROM article LEFT JOIN type ON article.type_id = type.Id '+
+  'WHERE article.id='+id
+  
+  const result = await this.app.mysql.query(sql)
+
+  this.ctx.body={data:result}
+
+}
+
+//得到类别名称和编号
+async getTypeInfo(){
+
+  const result = await this.app.mysql.select('type')
+  this.ctx.body = {data:result}
+
+}
+
+//根据类别ID获得文章列表
+async getListById(){
+  let id = this.ctx.params.id
+  let sql = 'SELECT article.id as id,'+
+  'article.title as title,'+
+  'article.introduce as introduce,'+
+  "FROM_UNIXTIME(article.addTime,'%Y-%m-%d') as addTime,"+
+  'article.view_count as view_count ,'+
+  'type.typeName as typeName '+
+  'FROM article LEFT JOIN type ON article.type_id = type.Id '+
+  'WHERE type_id='+id
+  const result = await this.app.mysql.query(sql)
+  this.ctx.body={data:result}
+}
+
+//根据文章ID增加点击量
+async addViewCount() {
+  const tmpArticle = this.ctx.request.body
+  const result = await this.app.mysql.update('article', tmpArticle);
+  const updateSuccess = result.affectedRows === 1;
+  this.ctx.body = {
+      isScuccess: updateSuccess
+  }
+}
+
+}
+
+module.exports = HomeController;
